9118d521ecb7de7abd796d3f99c9cc3a41efbcc8
Pradeep Sadashivamurthy <pradeepcheers@gmail.com>
2021-02-24 22:45:14 +0000

    Added currency conversion feature

--- src/service/marketsummary-service.js
+++ src/service/marketsummary-service.js
@@ -1,60 +1,18 @@
-const { Quote } = require("../repository/models");
+const { getMarketSummary } = require("../service/marketsummary-service");
+const { convertCurrency } = require("../service/currency-service");
+const { getLocaleFromRequest } = require("../util/helpers");
 
-const getMarketSummary = async () => {
-    const losers = await Quote().find(
-        {},
-        { _id: 0 },
-        { sort: { change1: 1 }, limit: 3 }
-    );
+const marketsummary = (server) => {
+    server.route({
+        method: "GET",
+        path: "/marketsummary",
+        handler: async (request, h) => {
+            const marketsummary = await getMarketSummary();
+            const locale = getLocaleFromRequest(request);
 
-    const winners = await Quote().find(
-        {},
-        { _id: 0 },
-        { sort: { change1: -1 }, limit: 3 }
-    );
-
-    const summary = await Quote().aggregate([
-        {
-            $facet: {
-                tradeStockIndexAverage: [
-                    { $group: { _id: null, value: { $avg: "$price" } } },
-                    { $project: { _id: 0, value: { $round: ["$value", 2] } } },
-                ],
-                tradeStockIndexOpenAverage: [
-                    { $group: { _id: null, value: { $avg: "$open1" } } },
-                    { $project: { _id: 0, value: { $round: ["$value", 2] } } },
-                ],
-                tradeStockIndexVolume: [
-                    { $group: { _id: null, value: { $sum: "$volume" } } },
-                    { $project: { _id: 0, value: 1 } },
-                ],
-                change: [
-                    { $group: { _id: null, value: { $sum: "$change1" } } },
-                    { $project: { _id: 0, value: 1 } },
-                ],
-            },
+            return convertCurrency(marketsummary, locale);
         },
-    ]);
-
-    const {
-        tradeStockIndexAverage,
-        tradeStockIndexOpenAverage,
-        tradeStockIndexVolume,
-        change,
-    } = summary[0];
-
-    return {
-        tradeStockIndexAverage: tradeStockIndexAverage[0].value,
-        tradeStockIndexVolume: tradeStockIndexVolume[0].value,
-        tradeStockIndexOpenAverage: tradeStockIndexOpenAverage[0].value,
-        topLosers: losers,
-        topGainers: winners,
-        summaryDate: new Date().toISOString(),
-        change: change[0].value,
-        percentGain: 0,
-    };
+    });
 };
 
-module.exports = {
-    getMarketSummary,
-};
+module.exports = marketsummary;
